9a4b2cdd30bf1ccce876b00eed9ec692b090f19c
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-12-06 17:52:23 -0500

    More meaningful variable names.

--- encrypt_test.go
+++ encrypt_test.go
@@ -6,18 +6,18 @@ import (
 
 func TestNewAccount(t *testing.T) {
 	data := []struct {
-		Email, Name string
+		Email, Hash string
 	}{
 		{"john@doe.com", "7wohs32cngzuqt466q3ge7indszva4of"},
 		{"jane@doe.com", "vuuf3eqgloxpxmzph27f5a6ve7gzlrms"},
 		{"john@example.com", "kismw365lo7emoxr3ohojgpild6lph4b"},
 	}
 
-	for _, info := range data {
-		acct := NewAccount(info.Email)
+	for _, user := range data {
+		acct := NewAccount(user.Email)
 
-		if info.Name != acct.String() {
-			t.Errorf("Expected %s, got %s.\n", info.Name, acct.String())
+		if user.Hash != acct.String() {
+			t.Errorf("Expected %s, got %s.\n", user.Hash, acct.String())
 		}
 	}
 }
