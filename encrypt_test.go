ac5431ba1df190a7bbe972d7cf891b2b796494e5
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-12-06 17:49:20 -0500

    Anonymous type for test.
    Didn't know Go could do that.
    Beautiful.

--- encrypt_test.go
+++ encrypt_test.go
@@ -5,20 +5,19 @@ import (
 )
 
 func TestNewAccount(t *testing.T) {
-	emails := []string{
-			"john@doe.com",
-			"jane@doe.com",
-			"john@example.com"}
-	names := []string{
-			"7wohs32cngzuqt466q3ge7indszva4of",
-			"vuuf3eqgloxpxmzph27f5a6ve7gzlrms",
-			"kismw365lo7emoxr3ohojgpild6lph4b"}
+	data := []struct {
+		Email, Name string
+	}{
+		{"john@doe.com", "7wohs32cngzuqt466q3ge7indszva4of"},
+		{"jane@doe.com", "vuuf3eqgloxpxmzph27f5a6ve7gzlrms"},
+		{"john@example.com", "kismw365lo7emoxr3ohojgpild6lph4b"},
+	}
 
-	for i, val := range emails {
-		acct := NewAccount(val)
+	for _, info := range data {
+		acct := NewAccount(info.Email)
 
-		if names[i] != acct.String() {
-			t.Errorf("Expected %s, got %s.\n", names[i], acct.String())
+		if info.Name != acct.String() {
+			t.Errorf("Expected %s, got %s.\n", info.Name, acct.String())
 		}
 	}
 }
