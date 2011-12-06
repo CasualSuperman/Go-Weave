c7fb9ea6516ce8f53e211a48a0fe40701f47c34d
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-12-06 01:24:18 -0500

    Last account test.

--- encrypt_test.go
+++ encrypt_test.go
@@ -7,10 +7,12 @@ import (
 func TestNewAccount(t *testing.T) {
 	emails := []string{
 			"john@example.com",
-			"john@doe.com"}
+			"john@doe.com",
+			"jane@doe.com"}
 	names := []string{
 			"kismw365lo7emoxr3ohojgpild6lph4b",
-			"7wohs32cngzuqt466q3ge7indszva4of"}
+			"7wohs32cngzuqt466q3ge7indszva4of",
+			"vuuf3eqgloxpxmzph27f5a6ve7gzlrms"}
 
 	for i, val := range emails {
 		acct := NewAccount(val)
